efb027fd92471e0066dc4be8f33c29f185a99658
Youssef <youssef.abouhamid@student.vinci.be>
2023-10-08 15:56:44 +0200

    Fixed pizzas.js

--- tutorials/basic/routes/pizzas.js
+++ tutorials/basic/routes/pizzas.js
@@ -1,35 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-const FILMS = [
+const MENU = [
 	{
-		"id": 1,
-		"title": "Inception",
-		"duration": 148,
-		"budget": 160,
-		"link": "https://www.imdb.com/title/tt1375666/"
+		id: 1,
+		title: '4 fromages',
+		content: 'Gruyère, Sérac, Appenzel, Gorgonzola, Tomates',
 	},
 	{
-		"id": 2,
-		"title": "The Dark Knight",
-		"duration": 152,
-		"budget": 185,
-		"link": "https://www.imdb.com/title/tt0468569/"
+		id: 2,
+		title: 'Vegan',
+		content: 'Tomates, Courgettes, Oignons, Aubergines, Poivrons',
 	},
 	{
-		"id": 3,
-		"title": "Avatar",
-		"duration": 162,
-		"budget": 237,
-		"link": "https://www.imdb.com/title/tt0499549/"
-	}
-]
-
+		id: 3,
+		title: 'Vegetarian',
+		content: 'Mozarella, Tomates, Oignons, Poivrons, Champignons, Olives',
+	},
+	{
+		id: 4,
+		title: 'Alpage',
+		content: 'Gruyère, Mozarella, Lardons, Tomates',
+	},
+	{
+		id: 5,
+		title: 'Diable',
+		content: 'Tomates, Mozarella, Chorizo piquant, Jalapenos',
+	},
+];
 
 // Read all the pizzas from the menu
 router.get('/', (req, res, next) => {
-	console.log('GET /films');
-	res.json(FILMS);
+	console.log('GET /pizzas');
+	res.json(MENU);
 });
 
 module.exports = router;
