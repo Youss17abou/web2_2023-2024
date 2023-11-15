a6e4f4553c2d378b698e8339a38b50a3d50d2f2c
Youssef <youssef.abouhamid@student.vinci.be>
2023-11-15 21:53:36 +0100

    2.8: dynamic HomePage

--- project/2.8/src/index.js
+++ project/2.8/src/index.js
@@ -1,2 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
+
+import grootImage from './img/groot.jpg'
+
+renderPageImage(grootImage);
+
+function renderPageImage(urlImage) {
+    const image = new Image();
+    image.src = urlImage;
+    image.height = 750;
+    const main = document.querySelector('main');
+    main.appendChild(image);
+}
