cb85faa684870e58771aab70ff915e9db378a72d
Youssef <youssef.abouhamid@student.vinci.be>
2024-01-17 01:00:50 +0100

    2.15 : view online jokes

--- exercises/2.15/src/Components/Pages/HomePage.js
+++ exercises/2.15/src/Components/Pages/HomePage.js
@@ -1,6 +1,25 @@
+import { clearPage } from "../../utils/render";
+
 const HomePage = () => {
-  const main = document.querySelector('main');
-  main.innerHTML = 'Deal with the content of your HomePage';
+  clearPage();
+/* eslint-disable linebreak-style */
+const main = document.querySelector('main');
+
+// Fetch a joke from JokeAPI
+fetch('https://v2.jokeapi.dev/joke/Any?type=single')
+  .then((response) => response.json())
+  .then((data) => {
+    // Display the joke and its category
+    const jokeText = document.createElement('p');
+    jokeText.textContent = `Joke :${data.joke}`;
+    main.appendChild(jokeText);
+
+    const jokeCategory = document.createElement('p');
+    jokeCategory.textContent = `Category: ${data.category}`;
+    main.appendChild(jokeCategory);
+  })
+  .catch((error) => console.error('Error fetching joke:', error));
+
 };
 
 export default HomePage;
